8e35c5576e79b8b2866a57af4618e012f66933e8
Aleksandr-tes <91590581+Aleksandr-tes@users.noreply.github.com>
2021-09-29 10:51:23 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+
+// let imp1 = document.querySelector('.imp-1');
+// let label1 = document.querySelector('.label-1');
+
+// imp1.addEventListener('click', labelForm);
+
+
+// function labelForm() {
+//    if (imp1.value >= 0) {
+//       // label1.style.color = '#FFD200' ; 
+//       label1.style.top = '-14px' ; 
+//       label1.style.background = '#ffffff'; 
+//       label1.style.padding = '0 7px'; 
+
+//    }
+// }
+
+
+
+
+// let imp2 = document.querySelector('.imp-2');
+// let label2 = document.querySelector('.label-2');
+
+// imp2.addEventListener('click', labelForm2);
+
+// function labelForm2() {
+//    if (imp2.value >= 0) {
+//       // label2.style.color = '#FFD200' ; 
+//       label2.style.top = '-14px' ; 
+//       label2.style.background = '#ffffff'; 
+//       label2.style.padding = '0 7px'; 
+
+//    }
+// }
